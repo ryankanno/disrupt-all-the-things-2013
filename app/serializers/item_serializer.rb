23f409842e7aebe64ab4569e7f8490c379318cd5
Ryan Kanno <ryankanno@localkinegrinds.com>
2013-04-27 19:55:55 -0400

    ADDED LAT LONG TO THE APISSS. ALLTHINGS API!

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,7 +1,15 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :heading, :description, :photo_url
+  attributes :id, :heading, :description, :photo_url, :latitude, :longitude
 
   def photo_url
     "#{object.photo.url}"
+  end
+
+  def latitude
+    "#{object.lonlat.y}"
+  end
+
+  def longitude
+    "#{object.lonlat.x}"
   end
 end
