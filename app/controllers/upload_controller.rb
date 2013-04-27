97c94d27b76fabac0e23ebc02354c2a8f0c8d5ad
Ryan Kanno <ryankanno@localkinegrinds.com>
2013-04-27 15:48:49 -0400

    ADDED ALL THE THINGS

--- app/controllers/upload_controller.rb
+++
@@ -1,6 +0,0 @@
-class UploadController < ApplicationController
-  def upload 
-    uploader = UploaderUploader.new
-    uploader.store!(my_file)
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define() do
+ActiveRecord::Schema.define(:version => 20130427192300) do
+
+  create_table "items", :force => true do |t|
+    t.string   "heading",                                                                 :default => ""
+    t.string   "description",                                                             :default => ""
+    t.spatial  "lonlat",      :limit => {:srid=>4326, :type=>"point", :geographic=>true}
+    t.datetime "created_at",                                                                              :null => false
+    t.datetime "updated_at",                                                                              :null => false
+  end
 
 end
