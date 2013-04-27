9aef018514fe7d760b11fddb57efcda6dfe4a680
Austen Ito <austen.ito@gmail.com>
2013-04-27 13:11:36 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RrrServer::Application.config.secret_token = '2244de0259f47e9aa4e7c3444b8422fc10d64e884d2c3ceebb94ae64f9af20d4b44dd447aa08a273504881fb82d70b588d87af48253bf26ddbf137126315e45b'
