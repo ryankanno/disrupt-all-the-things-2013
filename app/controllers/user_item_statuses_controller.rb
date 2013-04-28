ac11cefb5d3557b47cd5ea4497b31be0b2b05613
Austen Ito <austen.ito@gmail.com>
2013-04-28 06:25:49 -0400

    Status set on update

--- app/controllers/user_item_statuses_controller.rb
+++ app/controllers/user_item_statuses_controller.rb
@@ -2,22 +2,24 @@ class UserItemStatusesController < ApplicationController
 
   def update
     item_id = params[:item_id]
-    item_status = current_user.user_item_statuses.where(item_id: item_id).first
-    if item_status
-      item_status.update_attribute(:status, params[:status])
+    item_status = UserItemStatus.where(item_id: params[:id], user_id: current_user.id).first
+    unless item_status
+      item_status = UserItemStatus.new(:item_id => params[:id])
+      current_user.user_item_statuses << item_status
+    end
 
-      UserItemStatus.where(item_id: item_id).each do |status|
+    item_status.update_attribute(:status, params[:status])
+
+    if UserItemStatus::OMW == params[:status]
+      ["8082306540", "8082323326", "8082565013"].each do |number|
         TWILIO.account.sms.messages.create(
           :from => "+14692511904",
-          :to => "+1#{status.user.phone_number}",
-          :body => "rio://#{item_status.user.uid}"
+          :to => "+1#{number}",
+          :body => "rio://reuseon"
         )
       end
-
-      render json: item_status
-    else
-      render(json: {}, status: :not_found)
     end
-  end
 
+    render json: item_status
+  end
 end
